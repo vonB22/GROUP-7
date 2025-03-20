03ad4ec4a8b4b1bd397fe1c53e57d461a5bbf8d7
vonB22 <balanavonlloyd22@gmail.com>
2025-03-19 21:52:36 -0600

    Updated

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -12,36 +12,8 @@
 
   <li class="nav-item">
     <a class="nav-link collapsed" href="index.php">
-      <i class="fa-solid fa-music" style="color: black;"></i>
-      <span class="text-dark">Songs</span>
-    </a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="index.php">
-      <i class="bi bi-person-fill" style="color: black;"></i>
-      <span class="text-dark">Artist</span>
-    </a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="index.php">
-      <i class="fa-solid fa-images" style="color: black;"></i>
-      <span class="text-dark">Albums & Genres</span>
-    </a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="index.php">
       <i class="fa-solid fa-circle-user" style="color: black;"></i>
       <span class="text-dark">Profile</span>
-    </a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="index.php">
-      <i class="bi bi-music-note-list" style="color: black;"></i>
-      <span class="text-dark">Playlist</span>
     </a>
   </li>
 
